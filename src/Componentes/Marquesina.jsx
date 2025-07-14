f71d6dd2fa36b01a7bc275e075b4b18a567a1cbd
benja3086 <benjaminquinteros785@gmail.com>
2025-07-14 19:41:22 -0300

    agregado zona de notificaciones

--- src/Componentes/Marquesina.jsx
+++ src/Componentes/Marquesina.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Marquesina.css";
 
 const Marquesina = () => {
-  const texto = "Hasta en lo último de la tierra ㅤㅤㅤㅤ ";
+  const texto = "Hasta lo último de la tierra ㅤㅤㅤㅤ ";
 
   return (
     <div className="marquesina">
