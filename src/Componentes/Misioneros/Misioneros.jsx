7c3207d08d904b430be779163ecc29634508cf27
benja3086 <benjaminquinteros785@gmail.com>
2025-09-24 14:42:07 -0300

    corregir la ortografia de misioneros

--- src/Componentes/Misioneros/Misioneros.jsx
+++ src/Componentes/Misioneros/Misioneros.jsx
@@ -5,15 +5,15 @@ import "./Misioneros.css";
 const datosMisioneros = {
   familiarinaldi: {
     titulo: "Familia Rinaldi",
-    descripcion: "Su Historia\n Sirven desde el año 2012 en Amaicha del Valle, Tucumán.\nActualmente viven alli y estan dando a conocer a\n Jesus en los Valles Calchaquies a traves de\n relaciones intencionales con las personas\n Por la gracia de Dios ya existe una comunidad\nfirme de hermanos en crecimiento, siendo \n transformados por Jesus \n¿como orar? Por el pueblo de Amaicha y alrededores, por cada\n habitante que llegue al conocimiento de la verdad\n y Cristo sea revelado en sus vidas.\n Por los hermanos de la iglesia en Amaicha,Dios\n permita permanecer en El siendo de fiel testimonio.\n Por la familia Rinaldi, que Dios sea siempre su\n guia constante y puedan escuchar su voz ",
+    descripcion: "SU HISTORIA:\n Sirven desde el año 2012 en Amaicha del Valle.\n Actualmente viven allí y están dando a conocer a Jesús en los Valles Calchaquíes\n a través de relaciones intencionales con las personas.\n Por la gracia de Dios, ya existe una comunidad firme de hermanos en crecimiento, siendo transformados por Jesús. \n¿CÓMO ORAR?\n Por el pueblo de Amaicha y alrededores, por cada habitante que llegue al conocimiento de la verdad y Cristo sea revelado en sus vidas. Por los hermanos de la Iglesia en Amaicha, Dios permita permanecer en Él siendo de fiel testimonio. Por la familia Rinaldi, que Dios sea siempre su guía constante y puedan escuchar su voz.",
   },
   martitadeteri: {
     titulo: "Martita De Teri",
-    descripcion: "Misionera en colonia Caroya - Cordoba\n \n Fue encomendada como miisionera junto a su esposo Carlos (ya con el Señor) hace mas de 50 años.\n Sirviendo en Tinogasta, Pilar, y luego, en Colonia Caroya, donde fundadorn la iglesia hace mas de 35 años. Actualmente, escribe para la revista Joyas Escogidas, colabora con el ministerio femenino y otras actividades eclesiaticas\n\n OREMOS POR:\n\n Su salud, ya que es diabetica y sufre problemas de presion por su familia\n Provision de Dios para cada una de sus necesidades.\n Para que pueda ser un reflejo del amor de Dios en el grupo literario en el que participa",
+    descripcion: "MISIONERA EN COLONIA CAROYA - CÓRDOBA Fué encomendada como misionera junto a su esposo Carlos (ya con el Señor) hace más de 50 años. Sirviendo en Tinogasta, Pilar, y luego, en Colonia Caroya, donde fundaron la iglesia hace más de 35 años. Actualmente, escribe para la revista Joyas Escogidas, colabora con el ministerio femenino y otras actividades eclesiásticas. \nOREMOS POR:\n SU SALUD, YA QUE ES DIABETICA Y SUFRE PROBLEMAS DE PRESIÓN. POR SU FAMILIA PROVISIÓN DE DIOS PARA CADA UNA DE SUS NECESIDADES. PARA QUE PUEDA SER UN REFLEJO DEL AMOR DE DIOS EN EL GRUPO LITERARIO EN EL QUE PARTICIPA",
   },
    elviracorbalan: {
-    titulo: "Elvira Corbalan",
-    descripcion: "En el 2008, fue llamada a servir entre\n los Sos Kundi, pertenecientes a Papua Nueva Guinea.\n\n Actualmente es Asesora de Traduccion\n y Alfabetizacion, entre los Sos Kundi.\n Es profesora de Panorama Biblico y AT en el Centro de Entrenamiento de SIL",
+    titulo: "Elvira Corbalán",
+    descripcion: "En el año 2008, fue llamada a servir entre los Sos Kundi, pertenecientes a Papúa Nueva Guinea.\n Actualmente es Asesora de Traducción y Alfabetización, entre los Sos Kundi. Es profesora de Panorama Bíblico y Antiguo Testamento en el Centro de Entrenamiento de SIL",
   },
 };
 
