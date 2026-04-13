6b4c567ac6f9d85d54ae92e38501362814cf5860
benja3086 <benjaminquinteros785@gmail.com>
2026-04-13 15:00:15 -0300

    fix: cors para vercel

--- index.js
+++ index.js
@@ -19,7 +19,15 @@ console.log("✅ Conectado a MongoDB");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://tiendamisiones-ko965sdvq-benja3086s-projects.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
