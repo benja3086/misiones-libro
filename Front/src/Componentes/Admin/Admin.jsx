cb509903d1d9776c9a58242069afc3919c64562e
benja3086 <benjaminquinteros785@gmail.com>
2026-04-13 17:56:11 -0300

    error de ortografia arreglado4

--- Front/src/Componentes/Admin/Admin.jsx
+++ Front/src/Componentes/Admin/Admin.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "../../Context/AuthContext";
 import "./Admin.css";
 
 const API = import.meta.env.VITE_API_URL;
